238630db0b06c657d0eef05a30b5841cc83b120f
William Zhou <williamkzhou@hotmail.com>
2018-04-19 21:27:54 +0100

    Initial commit of project setup

---
+++ src/main/java/com/example/springdatarestpoc/SpringDataRestPocApplication.java
@@ -0,0 +1,12 @@
+package com.example.springdatarestpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRestPocApplication.class, args);
+	}
+}
